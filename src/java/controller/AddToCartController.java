32c0d7155981ed4b533b97653739afa5d48ce6ee
tinbusiness-work <tinbusiness.work@gmail.com>
2023-12-12 08:57:26 +0700

    AddToCartController.java

---
+++ src/java/controller/AddToCartController.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.DAO;
+import dto.Mobile;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author tinhtse173630
+ */
+public class AddToCartController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try {
+            String mobileId = request.getParameter("mobileId");
+            HttpSession session = request.getSession(true);
+            if (session != null) {
+                HashMap<String, Integer> cart = (HashMap<String, Integer>) session.getAttribute("cart");
+                if (cart == null) {
+                    cart = new HashMap<>();
+                    cart.put(mobileId, 1);
+                } else {
+                    Integer tmp = cart.get(mobileId);
+                    if (tmp == null) {
+                        cart.put(mobileId, 1);
+                    } else {
+                        tmp++;
+                        cart.put(mobileId, tmp);
+                    }
+                }
+                session.setAttribute("cart", cart);
+            }
+
+        } catch (Exception e) {
+        } finally {
+            request.getRequestDispatcher("SearchPriceController").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
