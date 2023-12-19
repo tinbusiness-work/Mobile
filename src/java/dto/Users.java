efc4e9568d2ff3c6cd497def95df12ba1d0933b7
tinbusiness-work <tinbusiness.work@gmail.com>
2023-12-19 13:56:55 +0700

    Users.java

---
+++ src/java/dto/Users.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ * 
+ * @author tinhtse173630
+ */
+public class Users {
+
+    String userID;
+    String password;
+    String fullName;
+    int role;
+
+    public Users() {
+    }
+
+    public Users(String userID, String password, String fullName, int role) {
+        this.userID = userID;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+}
