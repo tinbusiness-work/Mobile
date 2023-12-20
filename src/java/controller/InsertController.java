3db3bc28d948def91c8b41a3edaf8a403bd78cd0
tinbusiness-work <tinbusiness.work@gmail.com>
2023-12-20 16:43:54 +0700

    InsertController.java

---
+++ src/java/controller/InsertController.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.DAO;
+import dto.Mobile;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author tinhtse173630
+ */
+public class InsertController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        /* TODO output your page here. You may use following sample code. */
+        try {
+
+            String mobileId = request.getParameter("mobileId");
+            String description = request.getParameter("description");
+            float price = Float.parseFloat(request.getParameter("price"));
+            String mobileName = request.getParameter("mobileName");
+            int yearOfProduction = Integer.parseInt(request.getParameter("yearOfProduction"));
+            int quantity = Integer.parseInt(request.getParameter("quantity"));
+            boolean notSale = Boolean.parseBoolean(request.getParameter("notSale"));
+            DAO dao = new DAO();
+
+            Mobile c = new Mobile(mobileId, description, price, mobileName, yearOfProduction, quantity, notSale);
+
+            boolean checkInsert = dao.insert(c);
+
+            if (checkInsert) {
+                request.setAttribute("noti", "Save successfully");
+            } else {
+                request.setAttribute("noti", "Save failed");
+            }
+        } catch (Exception e) {
+        } finally {
+            request.getRequestDispatcher("SearchController").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
