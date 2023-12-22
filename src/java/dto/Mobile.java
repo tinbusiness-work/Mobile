d22dc7345ec64dec256335e0870a69f9b4541163
tinbusiness-work <tinbusiness.work@gmail.com>
2023-12-22 19:56:11 +0700

    Mobile.java

---
+++ src/java/dto/Mobile.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author tinhtse173630
+ */
+public class Mobile {
+
+    String mobileId;
+    String description;
+    float price;
+    String mobileName;
+    int yearOfProduction;
+    int quantity;
+    boolean notSale;
+
+    public Mobile() {
+    }
+
+    public Mobile(String mobileId, String description, float price, String mobileName, int yearOfProduction, int quantity, boolean notSale) {
+        this.mobileId = mobileId;
+        this.description = description;
+        this.price = price;
+        this.mobileName = mobileName;
+        this.yearOfProduction = yearOfProduction;
+        this.quantity = quantity;
+        this.notSale = notSale;
+    }
+
+    public String getMobileId() {
+        return mobileId;
+    }
+
+    public void setMobileId(String mobileId) {
+        this.mobileId = mobileId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getMobileName() {
+        return mobileName;
+    }
+
+    public void setMobileName(String mobileName) {
+        this.mobileName = mobileName;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isNotSale() {
+        return notSale;
+    }
+
+    public void setNotSale(boolean notSale) {
+        this.notSale = notSale;
+    }
+
+    
+
+}
